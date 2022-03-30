7e0be45a44821bbf107ce49e9c0b22ef880943e0
KIMBJ1 <qudwh9@naver.com>
2022-03-30 23:10:12 +0900

    Commit last version probono_back

---
+++ probono_back/src/main/java/dev/probono/probono/service/PersonService.java
@@ -0,0 +1,13 @@
+package dev.probono.probono.service;
+
+import java.util.List;
+
+import dev.probono.probono.model.dto.PersonDTO;
+
+public interface PersonService {
+    void insertPerson(PersonDTO personDTO);
+    List<PersonDTO> getAllPersons();
+    List<PersonDTO> getAllBeneficiaries();
+    List<PersonDTO> getAllDonators();
+    PersonDTO getOnePerson(Long personId);
+}
