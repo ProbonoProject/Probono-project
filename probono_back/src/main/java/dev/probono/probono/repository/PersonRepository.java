14a7fdd8171259018fedfa080fb7cce3ed261221
Sujin Jang <susan3722@snu.ac.kr>
2022-03-30 20:26:29 +0900

    back commit

--- probono_back/src/main/java/dev/probono/probono/repository/PersonRepository.java
+++ probono_back/src/main/java/dev/probono/probono/repository/PersonRepository.java
@@ -7,8 +7,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import dev.probono.probono.model.entity.Person;
 
 public interface PersonRepository extends JpaRepository<Person, Long>{
+<<<<<<< HEAD
     
     List<Person> findByListBenefitNotNull();
     List<Person> findByListDonationNotNull();
 
+=======
+    List<Person> findByListBenefitNotNull();
+    List<Person> findByListDonationNotNull();
+>>>>>>> b8b612827103d8c4787874b03cd8d60e2570dff1
 }
--- probono_back/src/test/java/dev/probono/probono/ProbonoApplicationTests.java
+++ probono_back/src/test/java/dev/probono/probono/ProbonoApplicationTests.java
@@ -1,18 +1,24 @@
 package dev.probono.probono;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+<<<<<<< HEAD
 import dev.probono.probono.model.entity.Match;
 import dev.probono.probono.model.entity.Person;
 import dev.probono.probono.model.entity.Talent;
+=======
+import dev.probono.probono.model.Matching;
+import dev.probono.probono.model.Person;
+import dev.probono.probono.model.Talent;
+>>>>>>> b8b612827103d8c4787874b03cd8d60e2570dff1
 import dev.probono.probono.repository.MatchRepository;
 import dev.probono.probono.repository.PersonRepository;
 import dev.probono.probono.repository.TalentRepository;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 @SpringBootTest
@@ -22,6 +28,7 @@ class ProbonoApplicationTests {
    void contextLoads() {
    }
 
+<<<<<<< HEAD
    @Autowired
    PersonRepository pr;
    
@@ -30,6 +37,34 @@ class ProbonoApplicationTests {
 
    @Autowired
    MatchRepository mr;
+=======
+	@Autowired
+	PersonRepository pr;
+	
+	@Autowired
+	TalentRepository tr;
+
+	@Autowired
+	MatchRepository mr;
+
+
+	@Test
+	public void saveTalent() {
+
+		Talent t1 = new Talent("talent_1");
+		tr.save(t1);
+
+		Talent t2 = new Talent("talent_2");
+		tr.save(t2);
+
+		Talent t3 = new Talent("talent_3");
+		tr.save(t3);
+
+	}
+
+	@Test
+	public void savePerson() {
+>>>>>>> b8b612827103d8c4787874b03cd8d60e2570dff1
 
    @Test
    public void saveTalent() {
@@ -74,6 +109,7 @@ class ProbonoApplicationTests {
 
 	@Test
 	public void saveMatch() {
+<<<<<<< HEAD
 		Match m1 = new Match(1L, 2L, 1L);
 		Match m2 = new Match(2L, 3L, 2L);
 		Match m3 = new Match(4L, 5L, 3L);
@@ -85,6 +121,19 @@ class ProbonoApplicationTests {
 
    @Test
    public void getAllTalents() {
+=======
+		Matching m1 = new Matching(1L, 2L, 1L);
+		mr.save(m1);
+		Matching m2 = new Matching(2L, 3L, 2L);
+		mr.save(m2);
+		Matching m3 = new Matching(4L, 5L, 3L);
+		mr.save(m3);
+	}
+
+
+	@Test
+	public void getAllTalents() {
+>>>>>>> b8b612827103d8c4787874b03cd8d60e2570dff1
 
       List<Talent> list = tr.findAll();
       
@@ -93,23 +142,36 @@ class ProbonoApplicationTests {
       }
    }
 
+<<<<<<< HEAD
    @Test
    public void getAllBeneficiaries() {
       List<Person> list = pr.findByListBenefitNotNull();
+=======
+	@Test
+	public void getAllBeneficiaries() {
+		List<Person> list = pr.findByListBenefitNotNull();
+>>>>>>> b8b612827103d8c4787874b03cd8d60e2570dff1
 
       for(Person p : list) {
          System.out.println(p.getId() + ", " + p.getName() + ", " + p.getListBenefit());
       }
    }
 
+<<<<<<< HEAD
    @Test
+=======
+	@Test
+>>>>>>> b8b612827103d8c4787874b03cd8d60e2570dff1
     public void getAllDonators() {
         List<Person> list = pr.findByListDonationNotNull();
         for(Person p : list) {
             System.out.println(p.getId() + ", " + p.getName() + ", " + p.getListDonation().get(0));
         }
     }
+<<<<<<< HEAD
 
 
+=======
+>>>>>>> b8b612827103d8c4787874b03cd8d60e2570dff1
 
 }
