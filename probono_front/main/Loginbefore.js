428e7c0d11fae95024ac41b914c37fa40017d7e0
KIMBJ1 <qudwh9@naver.com>
2022-04-01 00:26:42 +0900

    A day before the announcement2  - change css of footer

--- probono_front/main/Loginbefore.js
+++ probono_front/main/Loginbefore.js
@@ -3,213 +3,202 @@ var select = document.getElementById("talentList");
 var choice;
 var num;
 function printChoice() {
-    choice = select.options[select.selectedIndex].value;
-    // console.log(choice);
-    async function request() {
-        const response = await fetch(`http://localhost:8091/api/v1/persons/talent?talentId=${choice}`,
-        {
-            method: 'GET',
-        });
-        const data = await response.json();
-        console.log(data);
-    } 
-    request();
+	choice = select.options[select.selectedIndex].value;
+	// console.log(choice);
+	async function request() {
+		const response = await fetch(
+			`http://localhost:8091/api/v1/persons/talent?talentId=${choice}`,
+			{
+				method: "GET",
+			}
+		);
+		const data = await response.json();
+		console.log(data);
+	}
+	request();
 }
 function choiceNum() {
-    num = select.options[select.selectedIndex].value;
+	num = select.options[select.selectedIndex].value;
 
-    console.log(num);
+	console.log(num);
 
-    
-const url = "http://localhost:8091/api/v1/persons";
+	const url = "http://localhost:8091/api/v1/persons";
 
-// fetch(`${url}/donators`)
-//     .then(res => {
-//         return res.json();
-//     })
-//     .then(data => {
-//         console.log(data);
+	// fetch(`${url}/donators`)
+	//     .then(res => {
+	//         return res.json();
+	//     })
+	//     .then(data => {
+	//         console.log(data);
 
-//     });
+	//     });
 
-fetch(`${url}`)
-	.then((res) => {
-		return res.json();
-	})
-	.then((data) => {
-		console.log(data);
+	fetch(`${url}`)
+		.then((res) => {
+			return res.json();
+		})
+		.then((data) => {
+			console.log(data);
 
-        document.getElementById('table1').textContent = ''; // 요청마다 table 초기화
+			document.getElementById("talentInDonation").textContent = ""; // 요청마다 table 초기화
 
-		let table1 = document.createElement("table");
-		let thead1 = document.createElement("thead");
-		let tbody1 = document.createElement("tbody");
+			let talentInDonation = document.createElement("table");
+			let talentInDonationHead = document.createElement("thead");
+			let talentInDonationBody = document.createElement("tbody");
 
-		table1.appendChild(thead1);
-		table1.appendChild(tbody1);
+			talentInDonation.appendChild(talentInDonationHead);
+			talentInDonation.appendChild(talentInDonationBody);
 
-		// js에서 테이블 생성 by #table
-		document.getElementById("table1").appendChild(table1);
+			// js에서 테이블 생성 by #table
+			document.getElementById("talentInDonation").appendChild(talentInDonation);
 
-		// 테이블 헤더 생성
-		let row_head1 = document.createElement("tr");
-		let heading1_1 = document.createElement("th");
-		heading1_1.innerHTML = "name";
-		let heading1_2 = document.createElement("th");
-		heading1_2.innerHTML = "email";
-		let heading1_3 = document.createElement("th");
-		heading1_3.innerHTML = "donation";
-		let heading1_4 = document.createElement("th");
-		heading1_4.innerHTML = "donation name";
+			// 테이블 헤더 생성
+			let talentInDonationRow_head = document.createElement("tr");
+			let talentInDonationHeading_1 = document.createElement("th");
+			talentInDonationHeading_1.innerHTML = "name";
+			let talentInDonationHeading_2 = document.createElement("th");
+			talentInDonationHeading_2.innerHTML = "email";
+			let talentInDonationHeading_3 = document.createElement("th");
+			talentInDonationHeading_3.innerHTML = "donation";
+			let talentInDonationHeading_4 = document.createElement("th");
+			talentInDonationHeading_4.innerHTML = "donation name";
 
-		row_head1.appendChild(heading1_1);
-		row_head1.appendChild(heading1_2);
-		row_head1.appendChild(heading1_3);
-		row_head1.appendChild(heading1_4);
-		thead1.appendChild(row_head1);
-            
+			talentInDonationRow_head.appendChild(talentInDonationHeading_1);
+			talentInDonationRow_head.appendChild(talentInDonationHeading_2);
+			talentInDonationRow_head.appendChild(talentInDonationHeading_3);
+			talentInDonationRow_head.appendChild(talentInDonationHeading_4);
+			talentInDonationHead.appendChild(talentInDonationRow_head);
 
-            for (index = 0; index < data.length; index++) {
-                // if(data[index].donation == num || data[index].benefit == num) {
-                
-                let row_body = document.createElement("tr");
-                let row_body_data_1 = document.createElement("td");
-                row_body_data_1.innerHTML = data[index].name;
-                let row_body_data_2 = document.createElement("td");
-                row_body_data_2.innerHTML = data[index].email;
-                let row_body_data_3 = document.createElement("td");
-                row_body_data_3.innerHTML = num;
-                
-                if(data[index].donation == num) {
-    
-                let row_body_data_4 = document.createElement("td");
-                
-                // if(num == null) {
-                //     continue
-                // }
-                let listD;
-                let listB = data[index].benefit;
-    
-                if(num == 1) {
-                    listD = "국어"
-                } else if(num == 2) {
-                    listD = "수학"
-                } else if(num == 3) {
-                    listD = "영어"
-                } else if(num == 4) {
-                    listD = "피아노"
-                } else if(num == 5) {
-                    listD = "노래"
-                } else if(num == 6) {
-                    listD = "마술"
-                } else if(num == 7) {
-                    listD = "보건 및 의료"
-                } else if(num == 8) {
-                    listD = "무료법률상담"
-                } else if(num == 9) {
-                    listD = "청소년 상담"
-                } else if (num == 10) {
-                    listD = "JAVA 코딩 교육"
-                }
-    
-                row_body_data_4.innerHTML = listD;
-                
-                
-                row_body.appendChild(row_body_data_1);
-                row_body.appendChild(row_body_data_2);
-                row_body.appendChild(row_body_data_3);
-                row_body.appendChild(row_body_data_4);
-                tbody1.appendChild(row_body);
-            }
-        }
-        document.getElementById('table2').textContent = ''; // 요청마다 table 초기화
+			for (index = 0; index < data.length; index++) {
+				// if(data[index].donation == num || data[index].benefit == num) {
 
-		let table = document.createElement("table");
-		let thead = document.createElement("thead");
-		let tbody = document.createElement("tbody");
+				let row_body = document.createElement("tr");
+				let row_body_name = document.createElement("td");
+				row_body_name.innerHTML = data[index].name;
+				let row_body_email = document.createElement("td");
+				row_body_email.innerHTML = data[index].email;
+				let row_body_num = document.createElement("td");
+				row_body_num.innerHTML = num;
 
-		table.appendChild(thead);
-		table.appendChild(tbody);
+				if (data[index].donation == num) {
+					let row_body_donation = document.createElement("td");
 
-		// js에서 테이블 생성 by #table
-		document.getElementById("table2").appendChild(table);
+					// if(num == null) {
+					//     continue
+					// }
+					let listD;
 
-		// 테이블 헤더 생성
-		let row_head = document.createElement("tr");
-		let heading_1 = document.createElement("th");
-		heading_1.innerHTML = "name";
-		let heading_2 = document.createElement("th");
-		heading_2.innerHTML = "email";
-		let heading_3 = document.createElement("th");
-		heading_3.innerHTML = "benefit";
-		let heading_4 = document.createElement("th");
-		heading_4.innerHTML = "benefit name";
+					if (num == 1) {
+						listD = "국어";
+					} else if (num == 2) {
+						listD = "수학";
+					} else if (num == 3) {
+						listD = "영어";
+					} else if (num == 4) {
+						listD = "피아노";
+					} else if (num == 5) {
+						listD = "노래";
+					} else if (num == 6) {
+						listD = "마술";
+					} else if (num == 7) {
+						listD = "보건 및 의료";
+					} else if (num == 8) {
+						listD = "무료법률상담";
+					} else if (num == 9) {
+						listD = "청소년 상담";
+					} else if (num == 10) {
+						listD = "JAVA 코딩 교육";
+					}
 
-		row_head.appendChild(heading_1);
-		row_head.appendChild(heading_2);
-		row_head.appendChild(heading_3);
-		row_head.appendChild(heading_4);
-		thead.appendChild(row_head);
-            
-           
+					row_body_donation.innerHTML = listD;
 
-            for (index = 0; index < data.length; index++) {
-                // if(data[index].benefit == num || data[index].benefit == num) {
-                
-                let row_body = document.createElement("tr");
-                let row_body_data_1 = document.createElement("td");
-                row_body_data_1.innerHTML = data[index].name;
-                let row_body_data_2 = document.createElement("td");
-                row_body_data_2.innerHTML = data[index].email;
-                let row_body_data_3 = document.createElement("td");
-                row_body_data_3.innerHTML = num;
-                
-                if(data[index].benefit == num) {
-    
-                let row_body_data_4 = document.createElement("td");
-                
-                // if(num == null) {
-                //     continue
-                // }
-                let listD;
-                let listB = data[index].benefit;
-    
-                if(num == 1) {
-                    listD = "국어"
-                } else if(num == 2) {
-                    listD = "수학"
-                } else if(num == 3) {
-                    listD = "영어"
-                } else if(num == 4) {
-                    listD = "피아노"
-                } else if(num == 5) {
-                    listD = "노래"
-                } else if(num == 6) {
-                    listD = "마술"
-                } else if(num == 7) {
-                    listD = "보건 및 의료"
-                } else if(num == 8) {
-                    listD = "무료법률상담"
-                } else if(num == 9) {
-                    listD = "청소년 상담"
-                } else if (num == 10) {
-                    listD = "JAVA 코딩 교육"
-                }
-    
-                row_body_data_4.innerHTML = listD;
-                
-                
-                row_body.appendChild(row_body_data_1);
-                row_body.appendChild(row_body_data_2);
-                row_body.appendChild(row_body_data_3);
-                row_body.appendChild(row_body_data_4);
-                tbody.appendChild(row_body);
-            }
-        }
-		
-	});
+					row_body.appendChild(row_body_name);
+					row_body.appendChild(row_body_email);
+					row_body.appendChild(row_body_num);
+					row_body.appendChild(row_body_donation);
+					talentInDonationBody.appendChild(row_body);
+				}
+			}
+			document.getElementById("talentInBenefit").textContent = ""; // 요청마다 table 초기화
 
+			let talentInBenefit = document.createElement("table");
+			let talentInBenefitHead = document.createElement("thead");
+			let talentInBenefitBody = document.createElement("tbody");
+
+			talentInBenefit.appendChild(talentInBenefitHead);
+			talentInBenefit.appendChild(talentInBenefitBody);
+
+			// js에서 테이블 생성 by #table
+			document.getElementById("talentInBenefit").appendChild(talentInBenefit);
+
+			// 테이블 헤더 생성
+			let talentInBenefitRow_head = document.createElement("tr");
+			let talentInBenefitHead_1 = document.createElement("th");
+			talentInBenefitHead_1.innerHTML = "name";
+			let talentInBenefitHead_2 = document.createElement("th");
+			talentInBenefitHead_2.innerHTML = "email";
+			let talentInBenefitHead_3 = document.createElement("th");
+			talentInBenefitHead_3.innerHTML = "benefit";
+			let talentInBenefitHead_4 = document.createElement("th");
+			talentInBenefitHead_4.innerHTML = "benefit name";
+
+			talentInBenefitRow_head.appendChild(talentInBenefitHead_1);
+			talentInBenefitRow_head.appendChild(talentInBenefitHead_2);
+			talentInBenefitRow_head.appendChild(talentInBenefitHead_3);
+			talentInBenefitRow_head.appendChild(talentInBenefitHead_4);
+			talentInBenefitHead.appendChild(talentInBenefitRow_head);
+
+			for (index = 0; index < data.length; index++) {
+				// if(data[index].benefit == num || data[index].benefit == num) {
+
+				let row_body = document.createElement("tr");
+				let row_body_name = document.createElement("td");
+				row_body_name.innerHTML = data[index].name;
+				let row_body_email = document.createElement("td");
+				row_body_email.innerHTML = data[index].email;
+				let row_body_num = document.createElement("td");
+				row_body_num.innerHTML = num;
+
+				if (data[index].benefit == num) {
+					let row_body_benefit = document.createElement("td");
+
+					// if(num == null) {
+					//     continue
+					// }
+					let listB;
+
+					if (num == 1) {
+						listB = "국어";
+					} else if (num == 2) {
+						listB = "수학";
+					} else if (num == 3) {
+						listB = "영어";
+					} else if (num == 4) {
+						listB = "피아노";
+					} else if (num == 5) {
+						listB = "노래";
+					} else if (num == 6) {
+						listB = "마술";
+					} else if (num == 7) {
+						listB = "보건 및 의료";
+					} else if (num == 8) {
+						listB = "무료법률상담";
+					} else if (num == 9) {
+						listB = "청소년 상담";
+					} else if (num == 10) {
+						listB = "JAVA 코딩 교육";
+					}
+
+					row_body_benefit.innerHTML = listB;
+
+					row_body.appendChild(row_body_name);
+					row_body.appendChild(row_body_email);
+					row_body.appendChild(row_body_num);
+					row_body.appendChild(row_body_benefit);
+					talentInBenefitBody.appendChild(row_body);
+				}
+			}
+		});
 }
-btn.addEventListener('click', printChoice)
-btn.addEventListener('click', choiceNum)
-
+btn.addEventListener("click", printChoice);
+btn.addEventListener("click", choiceNum);
